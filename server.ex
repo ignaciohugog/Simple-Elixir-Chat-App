004a169b62230a5d8480eaa94f39d677fd47fb68
Ignacio H. Gomez <ignaciohugog@gmail.com>
2016-06-10 15:28:38 -0300

    - receive new users and storage

--- server.ex
+++ server.ex
@@ -1,3 +1,5 @@
+
+defmodule Server do 
 @doc """
 The server functions are:
 - receive new users 
@@ -5,15 +7,27 @@ The server functions are:
 - retrieve for each new user a list of availables users 
 - update users state to the already connected users 
   (user connects/disconnects)
-
-
-
 """
 
-defmodule Server do 
-
-	def incoming_user do
-		
+	def init do
+		spawn(Server, :incoming_user,[%{}])	
 	end
 
+	def incoming_user(users) do
+		receive do 
+			{from, user_name} ->	
+				IO.puts("New user!")
+				notifyUsers(users, {from, user_name})				
+				incoming_user(Dict.put_new(users, from, user_name))
+			end
+	end
+
+	def notifyUsers(users, newUser) do
+		#TODO
+		#for each users notify the new connection (new user)
+		IO.puts("notify users!, current:")
+		IO.inspect(users)
+		IO.inspect(newUser)
+	end
 end
+
