778930292c1f0e032777c4cfcb874233d6870a1f
Ignacio H. Gomez <ignaciohugog@gmail.com>
2016-06-10 14:09:52 -0300

    add server and client files

---
+++ client.ex
@@ -0,0 +1,18 @@
+@doc """
+The user functions are:
+- connects to a server
+- send msg
+- receive msg
+- notify seen
+- receive updates for current available users
+- read msg (short period of time)
+- answear msg (short period of time)
+
+- notify writing
+- mute user
+
+- create brodcast (talk with all available users)
+
+- notify finshing reading
+
+"""
---
+++ server.ex
@@ -0,0 +1,19 @@
+@doc """
+The server functions are:
+- receive new users 
+- storage new users
+- retrieve for each new user a list of availables users 
+- update users state to the already connected users 
+  (user connects/disconnects)
+
+
+
+"""
+
+defmodule Server do 
+
+	def incoming_user do
+		
+	end
+
+end
