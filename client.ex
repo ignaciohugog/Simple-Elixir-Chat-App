d27fd5f8a53b7ead4982c8ee6e5a60db8dafa148
Ignacio H. Gomez <ignaciohugog@gmail.com>
2016-06-13 19:18:34 -0300

    Finish Iterations

--- client.ex
+++ client.ex
@@ -1,39 +1,73 @@
 defmodule Client do
 @moduledoc """
-The user functions are:
 - connect to a server
-- chat
-- notify seen
-- read msg (short period of time)
-- answear msg (short period of time)
+- update users list
+- write
 - notify writing
+- read chats messages storaged
 - mute user
+- notify seen
 - create brodcast (talk with all available users)
-- notify finshing reading
 """
 
   def start do
-    spawn fn -> loop [] end
+    spawn fn -> loop %{}, %{}, %{} end
   end
 
-  def loop(users) do
+  def loop(users, chats, muted) do
     receive do
       {:connect, server, name} ->
         send server, {:connect, self, name}
-        loop users
+        loop users, chats, muted
 
       {:update, users} ->
-        IO.inspect(users)
-        loop users
+        loop users, chats, muted
 
-      {:send, from, msg} ->
-        IO.puts "[Client] #{Dict.get(users, from)} says: #{msg}"
-        send from, {:seen, self, "has seen [#{msg}]"}
-        loop users
+      {:brodcast, group, msg} ->
+        Enum.each(group, fn(u) -> send u, {:receive, self, msg} end)
+        loop users, chats, muted
+
+
+      {:write, recipient, msg} ->
+        send recipient, {:writting, Dict.get(users, self)}
+        :timer.sleep(10000)
+        send recipient, {:receive, self, msg}
+        loop users, chats, muted
+
+      {:writting, user} ->
+        IO.puts "[#{Dict.get(users, self)}] #{user} is writting"
+        loop users, chats, muted
+
+      {:receive, sender, msg} ->
+        if !Dict.has_key?(muted, sender), do:
+         IO.puts "[#{Dict.get(users, self)}] #{Dict.get(users, sender)} says: #{msg}"
+        send sender, {:sent, msg}
+        loop users, (Dict.put_new chats, sender, msg), muted
+
+      {:sent, msg} ->
+        IO.puts "[#{Dict.get(users, self)}] #{msg} received"
+
+      {:read} ->
+        if Enum.empty?(chats) do
+          IO.puts "[#{Dict.get(users, self)}] nothing to read"
+        else
+          {sender, msg} = Enum.random(chats)
+          IO.inspect("[#{Dict.get(users, self)}] reading #{Dict.get(users, sender)} messages")
+          send sender, {:seen, self, msg}
+          chats = Dict.delete(chats, sender)
+        end
+        loop users, chats, muted
 
       {:seen, from, msg} ->
-        IO.puts "[Client] #{Dict.get(users, from)}: #{msg}"
-        loop users
+        IO.puts "[#{Dict.get(users, self)}] #{Dict.get(users, from)} has seen: #{msg}"
+        loop users, chats, muted
+
+      {:mute, user} ->
+        loop users, chats, (Dict.put_new muted, user, "")
+
+      after 10_000 ->
+        send self, {:read}
+        loop users, chats, muted
     end
   end
 end
--- server.ex
+++ server.ex
@@ -1,13 +1,12 @@
 defmodule Server do
 @moduledoc """
-The server functions are:
 - receive new users
 - storage new users
 - update users list for each one
 """
 
   def start do
-    spawn fn -> loop(%{}) end
+    spawn fn -> loop %{} end
   end
 
 
@@ -23,12 +22,41 @@ The server functions are:
 end
 
 
-
+#--Basic test--
 # c("Server.ex")
 # c("Client.ex")
 # s = Server.start
 # c1 = Client.start
 # c2 = Client.start
+# c3 = Client.start
 # send c1, {:connect, s, "nam1"}
 # send c2, {:connect, s, "nam2"}
-# send c1, {:send, c2, "helloo"}
+# send c3, {:connect, s, "nam3"}
+# send c2, {:write, c1, "hello from c1"}
+# send c3, {:write, c1, "hello from c2"}
+
+#--Muted test--
+# c("Server.ex")
+# c("Client.ex")
+# s = Server.start
+# c1 = Client.start
+# c2 = Client.start
+# c3 = Client.start
+# send c1, {:connect, s, "nam1"}
+# send c2, {:connect, s, "nam2"}
+# send c3, {:connect, s, "nam3"}
+# send c1, {:mute, c2}
+# send c2, {:write, c1, "hello from c2"}
+
+#--Brodcast test--
+# c("Server.ex")
+# c("Client.ex")
+# s = Server.start
+# c1 = Client.start
+# c2 = Client.start
+# c3 = Client.start
+# send c1, {:connect, s, "nam1"}
+# send c2, {:connect, s, "nam2"}
+# send c3, {:connect, s, "nam3"}
+# send c1, {:brodcast, [c2,c3], "hello word"}
+
